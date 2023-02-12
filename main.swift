f905df58271b62ef402bd5d1c428b838108ed4c3
Craig Barrington <craig.barrington@outlook.com>
2023-02-12 17:56:24 +0000

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-let who = NSUserName()
+let fileName = "example.txt"
+let desktopPath = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
+let fileURL = desktopPath.appendingPathComponent(fileName)
 
-print(who)
+do {
+  try "This is some text".write(to: fileURL, atomically: true, encoding: .utf8)
+  print("File created at \(fileURL.path)")
+} catch {
+  print("Failed to create file")
+  print("\(error)")
+}
+
