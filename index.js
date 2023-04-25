72a7ba52fbcde392840a51b34feb22924fb402ab
Craig Barrington <craig.barrington@outlook.com>
2023-04-25 19:23:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-console.log("github.com")
+console.clear()
+
+const url = "https://api.github.com/users/bard";
+
+const response = await fetch(url);
+const data = await response.json();
+
+console.log(`
+
+Login:\t${data.login}
+url:\t${data.url}
+
+`);
