240caa70e8847b1363085a829d373a8f59420c2d
Craig Barrington <craig.barrington@outlook.com>
2023-07-15 17:15:37 +0100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,12 @@
+const url = "https://api.github.com/users/bard";
+
+const response = await fetch(url);
+const data = await response.json();
+
+console.log(`
+
+Login:\t${data.login}
+url:\t${data.url}
+
+`);
+
