0d6d81f84671cfebb0b7b2822652a063cea81b9e
Craig Barrington <craig.barrington@outlook.com>
2023-09-01 13:32:53 +0100

    Update scc.sh

--- scc.sh
+++ scc.sh
@@ -4,8 +4,8 @@ clear
 hdiutil attach http://02svrm001.scc.local/scc.dmg
 sleep 3
 if [ -d "/volumes/scc" ]; then
-  echo "connected to scc."
-  /volumes/scc/bootstrappr.sh
+  echo "connected to scc.";
+  sudo /volumes/scc/bootstrappr.sh
 else
 echo "ERROR! Volume not available"
 exit 1
