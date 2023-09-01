be3e8cb01a85596ba3fd0167d4ec49871c735657
Craig Barrington <craig.barrington@outlook.com>
2023-09-01 13:30:57 +0100

    Create scc.sh

---
+++ scc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+hdiutil attach http://02svrm001.scc.local/scc.dmg
+sleep 3
+if [ -d "/volumes/scc" ]; then
+  echo "connected to scc."
+  /volumes/scc/bootstrappr.sh
+else
+echo "ERROR! Volume not available"
+exit 1
+fi
+exit 0
