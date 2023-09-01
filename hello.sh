0d930563c1864d56e0b438006c20d93c96d11fb1
Craig Barrington <craig.barrington@outlook.com>
2023-09-01 13:14:49 +0100

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 clear
-hdiutil mount http://02svrm001.scc.local/adobe.dmg
+hdiutil attach http://02svrm001.scc.local/adobe.dmg
 sleep 3
 if [ -d "/volumes/adobe" ]; then
   echo "connected to volume."
@@ -10,5 +10,5 @@ echo "ERROR! Volume not available
 exit 1
 fi
 sleep 3
-hdiutil unmount http://02svrm001.scc.local/adobe.dmg
+hdiutil detach /volumes/Adobe\ 2023
 exit 0
