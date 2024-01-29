5eee9159c2d45ee6ede38ee298ba8a36089148b7
Craig Barrington <craig.barrington@outlook.com>
2024-01-29 14:25:51 +0000

    Update macInfo.sh

--- macInfo.sh
+++ macInfo.sh
@@ -8,10 +8,9 @@ echo " | |\/| |/ _  |/ __|  | ||  _ \| |_ / _ \  "
 echo " | |  | | (_| | (__   | || | | |  _| (_) | "
 echo " |_|  |_|\__,_|\___| |___|_| |_|_|  \___/  "
 echo "                                           "
-echo "         Craig Barrington © $DATE          "
+echo "         Craig Barrington © `date '+%Y'`   "
 
-
-DATE=`date '+%Y'`                                                   
+                                                 
 CPU_MODEL=`sysctl -n machdep.cpu.brand_string`;
 MODEL_ID=`sysctl -n hw.model`;
 MEMORY_SIZE=`sysctl hw.memsize | awk '{print $2 / 1024**3}'`;
